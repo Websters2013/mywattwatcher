ba517987a1de1e5c94c19dd40c1d9c94617a1d66
iversontb <petruninevgeniy@gmail.com>
2017-07-11 14:43:55 +0300

    site

---
+++ app/js/jquery.main.js
@@ -0,0 +1,57 @@
+"use strict";
+( function(){
+
+    window.onload = function () {
+
+        new Preloader( $('.preloader') );
+
+    };
+
+    $( function () {
+
+
+
+    } );
+
+    var Preloader = function (obj) {
+
+        //private properties
+        var _self = this,
+            _window = $( window ),
+            _html = $('html'),
+            _preloader = obj,
+            _body = $('body');
+
+        //private methods
+        var _init = function () {
+
+                _body[0].preloader = _self;
+                _showSite();
+
+            },
+            _showSite = function() {
+
+                _preloader.addClass( 'preloader_loaded' );
+
+                setTimeout( function() {
+
+                    _html.css( {
+                        'overflow-y': 'auto'
+                    } );
+
+                    _preloader.remove();
+
+                    $('.site').addClass( 'site__loaded' );
+
+                }, 500 );
+            };
+
+        //public properties
+
+        //public methods
+
+
+        _init();
+    };
+
+} )();
